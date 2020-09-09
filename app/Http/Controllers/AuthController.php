c13fb1db168417f40799a567862706f8d0647023
S. M. Jubayer <sadatjubayer@gmail.com>
2020-09-09 13:43:13 +0600

    login done

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -28,10 +28,31 @@ class AuthController extends Controller
             'password' => 'required|min:5'
         ]);
 
-        // Register here
-        // return view('auth.register');
-    }
 
+        $data = User::where('username', $request->username)
+            ->where('password', $request->password)
+            ->get();
+
+        if (count($data) > 0) {
+            $request->session()->put('username', $request->username);
+
+            if ($data[0]->role == "admin") {
+                $request->session()->put('role', "admin");
+                return redirect('admin');
+            } else if ($data[0]->role == "owner") {
+                $request->session()->put('role', "owner");
+                return redirect('owner');
+            } else if ($data[0]->role == "tenant") {
+                $request->session()->put('role', "tenant");
+                return redirect('tenant');
+            } else if ($data[0]->role == "employee") {
+                $request->session()->put('role', "employee");
+                return redirect('employee');
+            }
+        } else {
+            return view('auth.login')->withErrors('Invalid Credentials');
+        }
+    }
 
     public function register(AuthRequest $request)
     {
