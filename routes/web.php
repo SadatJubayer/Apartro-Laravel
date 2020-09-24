74adbed4e50ab457075bbb89e0fcc72f884c018d
foysalious <foysalrahman112@gmail.com>
2020-09-25 00:38:53 +0600

    Middleware and login solved

--- routes/web.php
+++ routes/web.php
@@ -9,12 +9,19 @@ Route::get('/', function () {
     return view('index');
 });
 
+
+
 // Auth Routes
 Route::get('/login', 'AuthController@loginView');
 Route::post('/login', 'AuthController@login');
 Route::get('/register', 'AuthController@registerView');
 Route::post('/register', 'AuthController@register');
 Route::get('/logout', 'AuthController@logout');
+
+
+Route::middleware(['tanentAccess'])->group(function () {
+Route::get('/tenant', 'TenantController@index');
+});
 
 // Admin Routes
 Route::middleware(['adminAccess'])->group(function () {
@@ -65,7 +72,7 @@ Route::middleware(['adminAccess'])->group(function () {
 
 
     // Admin profile
-    Route::get('/tenant', 'TenantController@index');
+   
 
 
 
