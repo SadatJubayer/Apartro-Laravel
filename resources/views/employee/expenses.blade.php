b454b170f0b0c98a0ef88abd508430d76bd6d3ea
nwanik <nasimwahid80@gmail.com>
2020-09-25 11:49:25 +0600

    add expenses done

--- resources/views/employee/expenses.blade.php
+++ resources/views/employee/expenses.blade.php
@@ -8,6 +8,11 @@ Expense
 
 @section('content')
 <div class="container">
+<div class="text-center my-3">
+        <button data-toggle="modal" data-target="#addExpense" class="btn btn-primary" role="button">
+            Add New Expenses
+        </button>
+    </div>
     <div class="row mt-5">
         <div class="col-md-10 mx-auto">
             <div class="h3 text-center my-2">All Expenses </div>
@@ -32,6 +37,37 @@ Expense
                     @endforeach
                 </tbody>
             </table>
+            <div class="modal fade" id="addExpense" aria-labelledby="addExpense" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header bg-primary text-white">
+                        <h5 class="modal-title">
+                            Add New Expense
+                        </h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <form action="/employee/expenses/new" method="POST">
+                            @csrf
+                            <div class="mb-3">
+                                <label for="description" class="form-label">Description</label>
+                                <input  required name="description" type="text" class="form-control" id="description" />
+                            </div>
+                            <div class="mb-3">
+                                <label for="cost" class="form-label">Cost</label>
+                                <input  required name="cost" type="text" class="form-control" id="cost" />
+                            </div>
+                            
+                            
+                            <button type="submit" class="btn btn-success text-center">
+                                Add New Expenses
+                            </button>
+                        </form>
+                    </div>
+           
+     </div>
             
         </div>
     </div>
